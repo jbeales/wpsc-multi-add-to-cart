7579189dad97c795f6e56512364beaf58ac2e734
John Beales <johnbeales@gmail.com>
2013-02-14 11:53:10 -0500

    Set up

---
+++ wpsc-multi-add-to-cart.php
@@ -0,0 +1,30 @@
+<?php
+
+
+/**
+ * @package Multi Add-to-Cart for WPEC
+ * @author John Beales (http://johnbeales.com)
+ * @version 0.1
+ */
+
+
+
+
+/*
+Plugin Name:Multi Add-to-Cart WPEC
+Plugin URI: http://johnbeales.com
+Description: Lets customers add multiple products to their shopping cart without having to visit a separate page for each product.
+Version: 0.1
+Author: John Beales
+Author URI: http://johnbeales.com
+*/
+
+
+
+
+
+
+
+
+
+?>
